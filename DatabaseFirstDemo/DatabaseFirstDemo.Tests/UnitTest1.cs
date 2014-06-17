f511d49e2a3af1811a7d86cb040b344831b58292
Manuel Riezebosch <mriezebosch@gmail.com>
2014-06-17 13:43:55 +0200

    voorbeeldje van proxy creation en lazy loading

--- DatabaseFirstDemo/DatabaseFirstDemo.Tests/UnitTest1.cs
+++ DatabaseFirstDemo/DatabaseFirstDemo.Tests/UnitTest1.cs
@@ -45,5 +45,51 @@ namespace DatabaseFirstDemo.Tests
                 Assert.IsTrue(context.People.OfType<Student>().Any(s => s.FirstName == "Gytis" && s.Grades.Any(g => g.Course.Name == "Composition" && g.Grade == 4)));
             }
         }
+
+        [TestMethod]
+        public void GivenInstructor_ShouldHave_Course()
+        {
+            using (var context = new SchoolEntities())
+            {
+                Assert.IsTrue(context.People.OfType<Instructor>().Any(s => s.PersonID == 1 && s.Course.Any(g => g.Name == "Chemistry")));
+            }
+        }
+
+        [TestMethod]
+        public void GivenInstructor_ShouldNotHave_Course()
+        {
+            using (var context = new SchoolEntities())
+            {
+                Assert.IsFalse(context.People.OfType<Instructor>().Any(s => s.PersonID == 5 && s.Course.Any(g => g.Name == "Chemistry")));
+            }
+        }
+
+        [TestMethod]
+        public void GivenInstructor_ShouldNotBe_DynamicProxy()
+        {
+            using (var context = new SchoolEntities())
+            {
+                context.Configuration.ProxyCreationEnabled = false;
+
+                var p = context.People.OfType<Instructor>().First();
+                Assert.AreEqual(typeof(Instructor), p.GetType());
+            }
+        }
+
+        [TestMethod]
+        public void GivenProxyCreationDisabled_Should_LazyLoadingNotWorking()
+        {
+            using (var context = new SchoolEntities())
+            {
+                context.Configuration.ProxyCreationEnabled = false;
+                context.Configuration.LazyLoadingEnabled = true;
+
+                // First instructor with courses
+                var p = context.People.OfType<Instructor>().Where(i => i.Course.Any()).First();
+                
+                // Courses not fetched because no proxy used
+                Assert.AreEqual(0, p.Course.Count);
+            }
+        }
     }
 }
