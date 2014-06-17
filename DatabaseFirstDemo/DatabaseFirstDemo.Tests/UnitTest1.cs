3cf67647d6b0a408adfd87cf52dabeb628f6d403
Manuel Riezebosch <mriezebosch@gmail.com>
2014-06-17 16:16:44 +0200

    create, update en delete van entities met transactionscope

--- DatabaseFirstDemo/DatabaseFirstDemo.Tests/UnitTest1.cs
+++ DatabaseFirstDemo/DatabaseFirstDemo.Tests/UnitTest1.cs
@@ -1,6 +1,11 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Linq;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity.Validation;
+using System.Data;
+using System.Data.Entity;
+using System.Transactions;
 
 namespace DatabaseFirstDemo.Tests
 {
@@ -86,9 +91,86 @@ namespace DatabaseFirstDemo.Tests
 
                 // First instructor with courses
                 var p = context.People.OfType<Instructor>().Where(i => i.Course.Any()).First();
-                
+
                 // Courses not fetched because no proxy used
                 Assert.AreEqual(0, p.Course.Count);
+            }
+        }
+
+        [TestMethod]
+        public void NotExistingInstructor_ShouldBe_Added()
+        {
+            // TransactionScope om onze test database niet te vervuilen. Handig!
+            using (var ts = new TransactionScope())
+            using (var context = new SchoolEntities())
+            {
+                var person = context
+                    .People
+                    .OfType<Student>()
+                    .FirstOrDefault(p => p.FirstName == "Pietje" && p.LastName == "Puk");
+
+                if (person == null)
+                {
+                    person = new Student
+                    {
+                        FirstName = "Pietje",
+                        LastName = "Puk",
+                        EnrollmentDate = DateTime.Today
+                    };
+
+                    context.People.Add(person);
+                }
+                else
+                {
+                    person.LastName = "Update";
+                }
+
+                try
+                {
+                    context.SaveChanges();
+                }
+                catch (DbEntityValidationException ex)
+                {
+                    Console.WriteLine(string.Join(Environment.NewLine, ex.EntityValidationErrors.SelectMany(e => e.ValidationErrors.Select(f => f.ErrorMessage))));
+                    Assert.Fail();
+                }
+            }
+        }
+
+        [TestMethod]
+        public void DetachedEntities_ShouldBe_AddedBeforeSaveChanges()
+        {
+            Person pietje = null;
+
+            // Die scope werkt dus ook over contexten heen!
+            using (new TransactionScope())
+            {
+                using (var context = new SchoolEntities())
+                {
+                    // AsNoTracking zorgt ervoor dat de ChangeTracker buitenspel wordt gezet.
+                    pietje = context.People.AsNoTracking().First(p => p.PersonID == 33);
+                }
+
+                pietje.LastName = "AS NO TRACKING";
+
+                using (var context = new SchoolEntities())
+                {
+                    // Slecht idee, twee pietjes in de DB
+                    //context.People.Add(pietje);
+
+                    context.Entry(pietje).State = EntityState.Modified;
+                    context.SaveChanges();
+                }
+            }
+        }
+
+        [TestMethod]
+        public void DeleteFromContext()
+        {
+            using (var context = new SchoolEntities())
+            {
+                context.People.RemoveRange(context.People.Where(p => p.FirstName == "Pietje"));
+                context.SaveChanges();
             }
         }
     }
