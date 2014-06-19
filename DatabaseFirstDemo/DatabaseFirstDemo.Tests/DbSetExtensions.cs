4f8c7c733dcc9228b54269e7c9d657f00aabba5e
Manuel Riezebosch <mriezebosch@gmail.com>
2014-06-19 11:33:15 +0200

    extension method en mock class erbij

---
+++ DatabaseFirstDemo/DatabaseFirstDemo.Tests/DbSetExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Moq;
+
+namespace DatabaseFirstDemo.Tests
+{
+    static class DbSetExtensions
+    {
+        public static void SetupMock<T>(this Mock<DbSet<T>> set, IQueryable<T> data)
+            where T: class
+        {
+            set.As<IQueryable<T>>().Setup(m => m.Provider).Returns(data.Provider);
+            set.As<IQueryable<T>>().Setup(m => m.Expression).Returns(data.Expression);
+            set.As<IQueryable<T>>().Setup(m => m.ElementType).Returns(data.ElementType);
+            set.As<IQueryable<T>>().Setup(m => m.GetEnumerator()).Returns(data.GetEnumerator());
+        }
+    }
+}
---
+++ DatabaseFirstDemo/DatabaseFirstDemo.Tests/SchoolsEntitiesMock.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabaseFirstDemo.Tests
+{
+    class SchoolsEntitiesMock : SchoolEntities
+    {
+        public override System.Data.Entity.DbSet<Person> People
+        {
+            get;
+            set;
+        }
+    }
+
+    class DbSetMock<T> : DbSet<T>
+    {
+
+    }
+}
